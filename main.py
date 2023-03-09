c46e597d473c31351b769051c9674a86feeaf082
Vladislav Kohai <cherry.designwork@gmail.com>
2023-03-09 16:08:03 +0300

    Task 7 is completed

--- main.py
+++ main.py
@@ -1,150 +1,141 @@
-# Количество различных чисел
+# Номер появления слова
 def task1():
-    nums = [int(i) for i in input().split()]
-    nums_set = set(nums)
-    print(len(nums_set))
+    my_dict = {}
+    lst = [i for i in input().split(' ')]
+    for i in lst:
+        if i in my_dict:
+            print(my_dict[i])
+            my_dict[i] += 1
+        else:
+            print(0)
+            my_dict[i] = 1
 
 
-# Количество совпадающих чисел
+# Словарь синонимов
 def task2():
-    nums1 = [int(i) for i in input().split()]
-    nums2 = [int(i) for i in input().split()]
-    print(len(set(nums1) & set(nums2)))
+    amount = int(input())
+    dictionary = {}
+    for i in range(amount):
+        word, synonym = input().split()
+        dictionary[word] = synonym
+        dictionary[synonym] = word
+    print(dictionary[input()])
 
 
-# Пересечение множеств
+# Выборы в США
 def task3():
-    nums1 = [int(i) for i in input().split()]
-    nums2 = [int(i) for i in input().split()]
-    nums = sorted(list(set(nums1) & set(nums2)))
-    for i in nums:
-        print(i)
+    amount_of_records = int(input())
+    members = {}
+    for i in range(amount_of_records):
+        last_name, voices = input().split()
+        voices = int(voices)
+        if last_name in members:
+            members[last_name] += voices
+            continue
+        members[last_name] = voices
+    for last_name, voices in sorted(members.items()):
+        print(last_name, voices)
 
 
-# Встречалось ли число раньше
+# Самое частое слово
 def task4():
-    nums1 = [int(i) for i in input().split()]
-    num_set = set()
-    for i in nums1:
-        if i in num_set:
-            print("YES")
-        else:
-            print("NO")
-            num_set.add(i)
+    words = {}
+    lines = int(input())
+    for i in range(lines):
+        line = input().split()
+        for word in line:
+            words[word] = words.get(word, 0) + 1
+    max_count = max(words.values())
+    most_frequent = [key for key, val in words.items() if val == max_count]
+    print(min(most_frequent))
 
 
-# Кубики
-def print_set(data):
-    print(len(data))
-    for i in sorted(list(data)):
-        print(i, end=' ')
-
-
+# Права доступа
 def task5():
-    n, m = [int(i) for i in input().split()]
-    anya, borya = set(), set()
-    for i in range(0, n):
-        anya.add(int(input()))
-    for i in range(0, m):
-        borya.add(int(input()))
+    history = []
+    files_count = int(input())
+    files_dict = {}
+    for _file in range(files_count):
+        # Вытаскиваем название файла и права
+        _data = input().split()
+        # Перебираем права
+        access_rights = set()
+        for right in range(1, len(_data)):
+            access_rights.add(_data[right].lower())
 
-    intersection_anya_set = anya & borya
-    deff_anya_set = anya - borya
-    deff_borya_set = borya - anya
+        # Заполняем словарь
+        files_dict[_data[0]] = access_rights
 
-    print_set(intersection_anya_set)
-    print_set(deff_anya_set)
-    print_set(deff_borya_set)
+    operations_count = int(input())
+    for operation in range(operations_count):
+        command, file_name = input().split()
+        # Проверяем доступ к файлу
+        if command[0] in files_dict[file_name] or (command[1] == 'x') and command[1] in files_dict[file_name]:
+            history.append("OK")
+            continue
+        history.append("Access denied")
+    print('\n'.join(history))
 
 
-# Количество слов в тексте
+# Частотный анализ
 def task6():
-    lines_lim = int(input())
-    text_set = set()
-    for i in range(lines_lim):
-        text_set.update(input().split())
-    print(len(text_set))
+    words = {}
+    for i in range(int(input())):
+        lines = input().split()
+        for word in lines:
+            words[word] = words.get(word, 0) + 1
+    for word in sorted(words.items(), key=lambda x: (-x[1], x[0])):
+        print(word[0])
 
 
-# Угадай число
+# Страны и города
 def task7():
-    n = int(input())
-    nums = set(range(1, n+1))
-    true_nums = nums
-    while True:
-        question = input()
-        if question == 'HELP':
-            break
-        question = {int(number) for number in question.split()}
-        respond = input()
-        if respond == "YES":
-            true_nums &= question
-        else:
-            true_nums &= nums - question
-    for i in sorted(list(true_nums)):
-        print(i)
+    world = {}
+    countries_count = int(input())
+    for i in range(countries_count):
+        line = input().split()
+        country = line[0]
+        cities = set()
+        for index in range(1, len(line)):
+            cities.add(line[index])
+        world[country] = cities
+    cities_count = int(input())
+    for i in range(cities_count):
+        city = input()
+        for key, value in world.items():
+            if city in value:
+                print(key)
 
 
-# Угадай число
+# Англо-латинский словарь
 def task8():
-    n = int(input())
-    nums = set(range(1, n+1))
-    true_nums = nums
-    while True:
-        question = input()
-        if question == 'HELP':
-            break
-        question = {int(number) for number in question.split()}
-        # respond = input()
-        if len(true_nums & question) > len(true_nums) / 2:
-            print("YES")
-            true_nums &= question
-        else:
-            print("NO")
-            true_nums &= nums - question
-    for i in sorted(list(true_nums)):
-        print(i)
+    pass
 
 
-# Полиглот
+# Контрольная по ударением
 def task9():
-    people = int(input())
-    all_languages = set()
-    general_languages = set()
-    for i in range(people):
-        amount_of_langs = int(input())
-        input_languages = set()
-        for i in range(amount_of_langs):
-            input_languages.add(input())
-            all_languages |= input_languages
-        general_languages = input_languages & all_languages
-
-    def print_sets(data):
-        print(len(data))
-        for i in sorted(data):
-            print(i)
-    print_sets(general_languages)
-    print_sets(all_languages)
+    pass
 
 
-# Забастовки
-# Der Kampf - Борьба
-# Die Arbeitstage - Будние дни
-# Der Tag - день
+# Продажи
 def task10():
-    # k - партия; n - дни
-    n, k = [int(i) for i in input().split()]
-    Arbeitstage = set(
-        [Tag for Tag in range(1, n + 1)
-         if Tag % 7 not in (6, 0)]
-    )
-    kein_kampf = set(Arbeitstage)
-    for partei in range(k):
-        a, b = [int(i) for i in input().split()]
-        max_kampf = (n - a)
-        kein_kampf -= {a + b*i for i in range(max_kampf)}
-    print(len(Arbeitstage) - len(kein_kampf))
+    pass
+
+
+# Родословная: подсчет уровней
+def task11():
+    pass
+
+
+# Родословная: предки и потомки
+def task12():
+    pass
+
+
+# Родословная: LCA
+def task13():
+    pass
 
 
 if __name__ == "__main__":
-    task10()
+    task7()
